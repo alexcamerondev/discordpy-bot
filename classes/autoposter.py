0ac59f66f86b4384f5b6bf8e97e6c844bbde2eaa
alexcamerondev <alexcameronprograms@gmail.com>
2021-01-05 15:05:35 -0400

    Clean up unused and useless comments

--- classes/autoposter.py
+++ classes/autoposter.py
@@ -32,40 +32,31 @@ class AutoPoster:
         self.news_next_in_queue = asyncio.Event()
         self.current_news = None
         self.news_topics = ['canada', 'aquaculture', 'environment', 'tesla', 'spacex', 'lobster','programming', 'reactjs', 'runescape','osrs', 'world of warcraft','hells kitchen', 'h3h3', 'pewdiepie' ] #Can improve this to be a dictionary later and populate from file.
-        # self.news_topics = ['canada'] #Can improve this to be a dictionary later and populate from file.
-
+ 
     async def random_news_topic(self):
         return str(random.choice(self.news_topics))
      
     async def auto_poster_loop(self):
         try:    
-            # queue_size = self.news_queue.qsize()
-            # queue_current_count = 0
-            # self.logger.log(f"Queue Size Start: {queue_size}")
             while self.auto_poster_queue_running_flag:
                 self.news_next_in_queue.clear()
                 self.current_news = await self.news_queue.get()
                 await self.channel_utility.send_to("meme",await self.api.cat_pictures()) #Send cat pictures to the meme channel
                 await self.channel_utility.send(await self.api.news(self.current_news['topic']))
-                await asyncio.sleep(self.__NEWS_INTERVAL)
+                await asyncio.sleep(self.FIFTEEN)
                 self.news_next_in_queue.set()
                 await self.news_next_in_queue.wait()
-                # queue_current_count += 1
                 if self.news_queue.empty():
                     self.clean_up()
-                # self.logger.log(f"Running Status after wait(): {self.is_running}")
         except Exception as e:
             self.logger.log(f"Exception auto_poster_loop :: {repr(e)}", Fore.RED)
            
     def clean_up(self):
         try:
-            # self.auto_poster_queue_running_flag = False    
             self.is_running = False
-            # self.news_next_in_queue.clear()#Clear our internal flags
             self.news_queue = asyncio.Queue() #Reset queue object.      
             self.news_next_in_queue = asyncio.Event()
-            self.current_news = None  
-            
+            self.current_news = None             
             self.logger.log(f"Clean up has run.") 
         except Exception as e:         
             self.logger.log(f"Exception occurred :: {repr(e)}", Fore.RED)  
